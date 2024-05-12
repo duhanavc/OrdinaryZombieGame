8b7d50305ad5d799cb2fe9ee497cc44e5bc692ea
Duhan Avcı <avciduhan@gmail.com>
2024-05-12 19:57:32 +0300

    working shoutgun but bullets angle is wrong

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,7 +4,7 @@ class_name Player
 
 @onready var playerAnimations = $AnimationPlayer
 const bulletScene = preload("res://Scenes/bullet.tscn")
-const shoutgunBulletScene = preload("res://Scenes/shoutgunBullet.tscn")
+#const shoutgunBulletScene = preload("res://Scenes/shoutgunBullet.tscn")
 var deagle = Deagle.new()
 
 var isFacingRight : bool = true
@@ -34,68 +34,50 @@ func _physics_process(delta):
 	if isFacingRight == true:
 		playerAnimations.play("walkRight")
 		$deagle.scale.y = 3.6 
+		$Shoutgun.scale.y = 3.34
 	if isFacingRight == false:
 		playerAnimations.play("walkLeft")
 		$deagle.scale.y = -3.6 
+		$Shoutgun.scale.y = -3.34
 	
 	
 func _process(delta):
 	$deagle.look_at(get_global_mouse_position())
 	$Shoutgun.look_at(get_global_mouse_position())
 	if Input.is_action_just_pressed("attack"):
-		if canAttack == true:
+		#if canAttack == true:
 			$deagle.deagleAnim.play()
 			$AudioStreamPlayer2D.play()
+			#canAttack = false
 			shootShoutgun()
-			shoot()
-		canAttack = false
+			#shoot()
 
-func shoot():
-	$pistolAttackSpeed.start()
-	var bullet = bulletScene.instantiate()
-	get_parent().add_child(bullet)
-	bullet.position = $deagle/BulletPos.global_position
-	bullet.Velocity = get_global_mouse_position() - bullet.position
-	bullet.look_at(get_global_mouse_position())
-	
+#func shoot():
+	#$pistolAttackSpeed.start()
+	#var bullet = bulletScene.instantiate()
+	#get_parent().add_child(bullet)
+	#bullet.position = $deagle/BulletPos.global_position
+	#bullet.Velocity = get_global_mouse_position() - bullet.position
+	#bullet.look_at(get_global_mouse_position())
+	#
 func shootShoutgun():
 	$ShoutgunAttackSpeed.start()
-	var ShoutgunBulletMid = shoutgunBulletScene.instantiate()
-	#var ShoutgunBulletTop = bulletScene.instantiate()
-	#var ShoutgunBulletBottom = bulletScene.instantiate()
+	var ShoutgunBulletMid = bulletScene.instantiate()
+	var ShoutgunBulletTop = bulletScene.instantiate()
+	var ShoutgunBulletBottom = bulletScene.instantiate()
+	
+
 	get_parent().add_child(ShoutgunBulletMid)
-	#get_parent().add_child(ShoutgunBulletTop)
-	#get_parent().add_child(ShoutgunBulletBottom)
+	get_parent().add_child(ShoutgunBulletTop)
+	get_parent().add_child(ShoutgunBulletBottom)
 	ShoutgunBulletMid.position = $Shoutgun/ShoutgunPos.global_position
-	#ShoutgunBulletTop.position = $ShoutgunPos.global_position
-	#ShoutgunBulletBottom.position = $ShoutgunPos.global_position
+	ShoutgunBulletTop.position = $Shoutgun/ShoutgunPos.global_position
+	ShoutgunBulletBottom.position = $Shoutgun/ShoutgunPos.global_position
 	#
-	ShoutgunBulletMid.Velocity = get_global_mouse_position() - ShoutgunBulletMid.position
-	#ShoutgunBulletTop.Velocity = get_global_mouse_position() - ShoutgunBulletMid.position
-	#ShoutgunBulletBottom.Velocity = get_global_mouse_position() - ShoutgunBulletMid.position
+	ShoutgunBulletMid.Velocity = get_global_mouse_position() - ShoutgunBulletMid.position ## direction al
+	ShoutgunBulletTop.Velocity = $Shoutgun/ShoutgunPos/ShoutgunPosUp.global_position - $Shoutgun/ShoutgunPos.global_position ## direction al
+	ShoutgunBulletBottom.Velocity = $Shoutgun/ShoutgunPos/ShoutgunPosDown.global_position -$Shoutgun/ShoutgunPos.global_position
+	# en kötü bot ve top için iki final marker ekle directionu o şekilde de alabilirsin ortadaki hala bizi döndürür
+	ShoutgunBulletTop.rotate(-60)
+	ShoutgunBulletBottom.rotate(60)
 	
-	ShoutgunBulletMid.look_at(get_global_mouse_position())
-	
-	#func hurtZombie():
-	#$AudioStreamPlayer2D.play()
-	#$"../HUD/hpBar".value -= 0.5
-	#if $"../HUD/hpBar".value == 0:
-		#maxhealth - se
-		#get_tree().reload_current_scene()
-###
-	#func healed():
-	#if $"../HUD/hpBar".value == 3:
-		#return false
-	#else:
-		#$"../HUD/hpBar".value += 0.5
-		#return true
-	
-
-
-func _on_pistol_attack_speed_timeout():
-	canAttack = true
-	
-
-
-func _on_shoutgun_attack_speed_timeout():
-	canAttack = true
